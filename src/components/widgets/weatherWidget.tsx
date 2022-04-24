dc8cb52ff6421ea9e0211788f4fab49b3e37287d
Bálint Berente <balint.berente@gmail.com>
2022-04-24 17:11:47 +0200

    Weather ID 7xx icon fix

--- src/components/widgets/weatherWidget.tsx
+++ src/components/widgets/weatherWidget.tsx
@@ -4,6 +4,7 @@ import styled from "styled-components";
 import { WeatherIcon } from "weather-react-icons";
 import "weather-react-icons/lib/css/weather-icons.css";
 import { colors } from "../../theme/theme";
+import { Mist } from "@styled-icons/remix-fill/Mist";
 
 export function WeatherWidget() {
   const { weather, error } = useWeather();
@@ -13,14 +14,20 @@ export function WeatherWidget() {
   }
   return (
     <Widget>
-      <StyledWeather
-        iconId={weather?.weather?.[0].id || 800}
-        name="owm"
-        night={
-          window.matchMedia &&
-          window.matchMedia("(prefers-color-scheme: dark)").matches
-        }
-      />
+      {weather?.weather?.[0].id &&
+      weather.weather[0].id < 800 &&
+      weather.weather[0].id >= 700 ? (
+        <StyledMist />
+      ) : (
+        <StyledWeather
+          iconId={weather?.weather?.[0].id || 800}
+          name="owm"
+          night={
+            window.matchMedia &&
+            window.matchMedia("(prefers-color-scheme: dark)").matches
+          }
+        />
+      )}
       <WidgetText>
         {Math.round(weather.main.temp) - 273}°
         {(weather.snow || weather.rain) &&
@@ -31,6 +38,12 @@ export function WeatherWidget() {
 }
 
 const StyledWeather = styled(WeatherIcon)`
+  height: 120px;
+  font-size: 100px;
+  fill: ${colors.theme};
+`;
+
+const StyledMist = styled(Mist)`
   height: 120px;
   font-size: 100px;
   fill: ${colors.theme};
